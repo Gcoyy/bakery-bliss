2929790792986a5cdc77017f1c8b64bca51cd24e
_kei_ <116942403+Gcoyy@users.noreply.github.com>
2025-07-11 22:46:54 +0800

    Added Framer Motion

--- src/sections/CakeCustom.jsx
+++ src/sections/CakeCustom.jsx
@@ -1,27 +1,28 @@
-import { useEffect, useRef, useState } from "react";
+// import { useEffect, useRef, useState } from "react";
+import { motion } from "motion/react"
 
 const CakeCustom = () => {
-  const buttonRef = useRef(null);
-  const [visible, setVisible] = useState(false);
+  // const buttonRef = useRef(null);
+  // const [visible, setVisible] = useState(false);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setTimeout(() => setVisible(true), 500);
-        }
-      },
-      { threshold: 0.5 }
-    );
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(
+  //     ([entry]) => {
+  //       if (entry.isIntersecting) {
+  //         setTimeout(() => setVisible(true), 500);
+  //       }
+  //     },
+  //     { threshold: 0.5 }
+  //   );
 
-    if (buttonRef.current) {
-      observer.observe(buttonRef.current);
-    }
+  //   if (buttonRef.current) {
+  //     observer.observe(buttonRef.current);
+  //   }
 
-    return () => {
-      if (buttonRef.current) observer.unobserve(buttonRef.current);
-    };
-  }, []);
+  //   return () => {
+  //     if (buttonRef.current) observer.unobserve(buttonRef.current);
+  //   };
+  // }, []);
 
 
   return (
@@ -43,48 +44,103 @@ const CakeCustom = () => {
     <img src="/image 4.png" alt="Cake" className="w-full mx-auto" />
 
     {/* Button positions */}
-    <button
-      ref={buttonRef}
-      className={`absolute top-[18%] left-[0%] text-gray-800 px-8 py-1.5 rounded-full text-2xl shadow-md tracking-[0.15em] max-w-xs w-auto text-left bg-gradient-to-r from-white/60 to-transparent text-[#3A3A3A] border-4 border-[#82171C] backdrop-filter backdrop-blur-[2px] font-semibold transition-opacity duration-700 ease-in
-        ${visible ? "opacity-100" : "opacity-0"}
-      `}
-    >
+    <motion.button
+    initial={{ opacity: 0, scale: 0 }}
+    whileInView={{ opacity: 1, scale: 1 }}
+    whileHover={{
+      scale: 1.1,
+      transition: { duration: 0.3 }
+    }}
+    transition={{
+      duration: 1.5,
+      ease: "easeInOut"
+    }}
+    viewport={{ once: true, amount: 0.5 }} // only plays once when 50% in view
+    className="absolute top-[18%] left-[0%] text-gray-800 px-8 py-1.5 rounded-full text-2xl shadow-md tracking-[0.15em] max-w-xs w-auto text-left bg-gradient-to-r from-white/60 to-transparent text-[#3A3A3A] border-4 border-[#82171C] backdrop-filter backdrop-blur-[2px] font-semibold">
       Add Frosting <br />& Fillings
-    </button>
+    </motion.button>
 
 
-    <button className={`
-    absolute top-[10%] right-[5%] 
-    text-gray-800 px-8 py-1.5 rounded-full text-2xl shadow-md tracking-[0.15em]
-    max-w-xs w-auto text-left
-    bg-gradient-to-l from-white/60 to-transparent text-[#3A3A3A]
-    border-4 border-[#82171C] backdrop-filter backdrop-blur-[2px]
-    font-semibold
-
-    transition-opacity duration-[700ms] ease-in 
-    transition-transform duration-[300ms] hover:scale-115 
-
-    ${visible ? "opacity-100" : "opacity-0"}
-    `}
+    <motion.button 
+    initial={{ opacity: 0, scale: 0 }}
+    whileInView={{ opacity: 1, scale: 1 }}
+    whileHover={{
+      scale: 1.1,
+      transition: { duration: 0.3 }
+    }}
+    transition={{
+      duration: 1.5,
+      ease: "easeInOut"
+    }}
+    viewport={{ once: true, amount: 0.5 }} // only plays once when 50% in view
+    className="absolute top-[10%] right-[5%] text-gray-800 px-8 py-1.5 rounded-full text-2xl shadow-md tracking-[0.15em] max-w-xs w-auto text-left bg-gradient-to-l from-white/60 to-transparent text-[#3A3A3A] border-4 border-[#82171C] backdrop-filter backdrop-blur-[2px] font-semibold"
     >
       Add Decorations <br />& Toppings
-    </button>
+    </motion.button>
 
-    <button className="absolute top-[42%] left-[-8%] text-gray-800 px-8 py-1.5 rounded-full text-2xl shadow-md tracking-[0.15em] max-w-fit w-auto text-left bg-gradient-to-r from-white/60 to-transparent text-[#3A3A3A] border-4 border-[#DFAD56]/53 transition-transform hover:scale-115 duration-600 delay-100 backdrop-filter backdrop-blur-[2px] font-semibold">
+    <motion.button 
+    initial={{ opacity: 0, scale: 0 }}
+    whileInView={{ opacity: 1, scale: 1 }}
+    whileHover={{
+      scale: 1.1,
+      transition: { duration: 0.3 }
+    }}
+    transition={{
+      duration: 1.5,
+      ease: "easeInOut"
+    }}
+    viewport={{ once: true, amount: 0.5 }} // only plays once when 50% in view
+    className="absolute top-[42%] left-[-8%] text-gray-800 px-8 py-1.5 rounded-full text-2xl shadow-md tracking-[0.15em] max-w-fit w-auto text-left bg-gradient-to-r from-white/60 to-transparent text-[#3A3A3A] border-4 border-[#DFAD56]/53 backdrop-filter backdrop-blur-[2px] font-semibold">
       Choose Cake <br />Flavor
-    </button>
+    </motion.button>
 
-    <button className="absolute top-[42%] right-[-8%] text-gray-800 px-8 py-1.5 rounded-full text-2xl shadow-md tracking-[0.15em] max-w-fit w-auto text-left bg-gradient-to-l from-white/60 to-transparent text-[#3A3A3A] border-4 border-[#DFAD56]/53 transition-transform hover:scale-115 duration-600 delay-100 backdrop-filter backdrop-blur-[2px] font-semibold">
+    <motion.button 
+    initial={{ opacity: 0, scale: 0 }}
+    whileInView={{ opacity: 1, scale: 1 }}
+    whileHover={{
+      scale: 1.1,
+      transition: { duration: 0.3 }
+    }}
+    transition={{
+      duration: 1.5,
+      ease: "easeInOut"
+    }}
+    viewport={{ once: true, amount: 0.5 }} // only plays once when 50% in view
+    className="absolute top-[42%] right-[-8%] text-gray-800 px-8 py-1.5 rounded-full text-2xl shadow-md tracking-[0.15em] max-w-fit w-auto text-left bg-gradient-to-l from-white/60 to-transparent text-[#3A3A3A] border-4 border-[#DFAD56]/53 transition-transform hover:scale-115 duration-600 delay-100 backdrop-filter backdrop-blur-[2px] font-semibold">
       Choose Cake <br />Layers
-    </button>
+    </motion.button>
 
-    <button className="absolute bottom-[20%] left-[-7%] text-gray-800 px-8 py-1.5 rounded-full text-2xl shadow-md tracking-[0.15em] max-w-fit w-auto text-left bg-gradient-to-r from-white/60 to-transparent text-[#3A3A3A] border-4 border-[#DFAD56]/53 transition-transform hover:scale-115 duration-600 delay-100 backdrop-filter backdrop-blur-[2px] font-semibold">
+    <motion.button 
+    initial={{ opacity: 0, scale: 0 }}
+    whileInView={{ opacity: 1, scale: 1 }}
+    whileHover={{
+      scale: 1.1,
+      transition: { duration: 0.3 }
+    }}
+    transition={{
+      duration: 1.5,
+      ease: "easeInOut"
+    }}
+    viewport={{ once: true, amount: 0.5 }} // only plays once when 50% in view
+    className="absolute bottom-[20%] left-[-7%] text-gray-800 px-8 py-1.5 rounded-full text-2xl shadow-md tracking-[0.15em] max-w-fit w-auto text-left bg-gradient-to-r from-white/60 to-transparent text-[#3A3A3A] border-4 border-[#DFAD56]/53 transition-transform hover:scale-115 duration-600 delay-100 backdrop-filter backdrop-blur-[2px] font-semibold">
       Choose Cake <br />Shape
-    </button>
+    </motion.button>
 
-    <button className="absolute bottom-[20%] right-[-12%] text-gray-800 px-8 py-1.5 rounded-full text-2xl shadow-md tracking-[0.15em] max-w-fit w-auto text-left bg-gradient-to-l from-white/60 to-transparent text-[#3A3A3A] border-4 border-[#DFAD56]/53 transition-transform hover:scale-115 duration-600 delay-100 backdrop-filter backdrop-blur-[2px] font-semibold">
+    <motion.button 
+    initial={{ opacity: 0, scale: 0 }}
+    whileInView={{ opacity: 1, scale: 1 }}
+    whileHover={{
+      scale: 1.1,
+      transition: { duration: 0.3 }
+    }}
+    transition={{
+      duration: 1.5,
+      ease: "easeInOut"
+    }}
+    viewport={{ once: true, amount: 0.5 }} // only plays once when 50% in view
+    className="absolute bottom-[20%] right-[-12%] text-gray-800 px-8 py-1.5 rounded-full text-2xl shadow-md tracking-[0.15em] max-w-fit w-auto text-left bg-gradient-to-l from-white/60 to-transparent text-[#3A3A3A] border-4 border-[#DFAD56]/53 transition-transform hover:scale-115 duration-600 delay-100 backdrop-filter backdrop-blur-[2px] font-semibold">
       Choose Cake <br />Size
-    </button>
+    </motion.button>
   </div>
 </section>
 
